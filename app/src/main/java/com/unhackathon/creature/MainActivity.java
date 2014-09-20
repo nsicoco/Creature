89c95407ce06ff03873e52ad8ed60f9fa42a252a
Noel Sicoco <nmsicoco@gmail.com>
2014-09-20 14:54:20 -0400

    Added CreatureCreationActivity It can take a picture and save it into new File(Environment.getExternalStorageDirectory(), "Creature")

--- app/src/main/java/com/unhackathon/creature/MainActivity.java
+++ app/src/main/java/com/unhackathon/creature/MainActivity.java
@@ -1,6 +1,7 @@
 package com.unhackathon.creature;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +13,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, CreatureCreationActivity.class));
     }
 
 
