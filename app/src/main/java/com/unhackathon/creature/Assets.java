8a2ca42885acf8ebb98e0a2043178f84a12540b2
francisrohner <francis.rohner@outlook.com>
2014-09-20 16:53:34 -0400

    Kilobit framework implemented, crashing until we add images to filesystem.

---
+++ app/src/main/java/com/unhackathon/creature/Assets.java
@@ -0,0 +1,12 @@
+package com.unhackathon.creature;
+
+import com.kilobolt.framework.Image;
+import com.kilobolt.framework.Sound;
+
+/**
+ * Created by francis on 9/20/14.
+ */
+public class Assets {
+    public static Image menu;
+    public static Sound click;
+}
---
+++ app/src/main/java/com/unhackathon/creature/screens/LoadingScreen.java
@@ -0,0 +1,66 @@
+package com.unhackathon.creature.screens;
+
+/**
+ * Created by francis on 9/20/14.
+ */
+import com.kilobolt.framework.Game;
+import com.kilobolt.framework.Graphics;
+import com.kilobolt.framework.Screen;
+import com.kilobolt.framework.Graphics.ImageFormat;
+import com.unhackathon.creature.Assets;
+
+
+public class LoadingScreen extends Screen {
+    public LoadingScreen(Game game) {
+        super(game);
+    }
+
+
+    @Override
+    public void update(float deltaTime) {
+        Graphics g = game.getGraphics();
+        Assets.menu = g.newImage("ic_launcher.png", ImageFormat.RGB565);
+        //Assets.click = game.getAudio().createSound("explode.ogg");
+
+
+
+        game.setScreen(new MainMenuScreen(game));
+
+
+    }
+
+
+    @Override
+    public void paint(float deltaTime) {
+
+
+    }
+
+
+    @Override
+    public void pause() {
+
+
+    }
+
+
+    @Override
+    public void resume() {
+
+
+    }
+
+
+    @Override
+    public void dispose() {
+
+
+    }
+
+
+    @Override
+    public void backButton() {
+
+
+    }
+}
